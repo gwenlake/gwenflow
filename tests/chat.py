65bb0055ffd3a1f4e203ebaa187f93be8afb02c9
Sylvain Barthélémy <sylbarth@gmail.com>
2024-11-11 00:37:18 +0100

    Update chat.py

--- tests/chat.py
+++ tests/chat.py
@@ -10,17 +10,17 @@ messages = [
     }
 ]
 
-# # No streaming
+# No streaming
 llm = OpenAI(model="gpt-4o-mini")
 response = llm.chat(messages=messages)
 print("")
 print(response["choices"][0]["message"]["content"])
 
-# # # Streaming
-# stream = llm.stream(messages=messages)
-# for chunk in stream:
-#     if chunk["choices"][0]["delta"]["content"]:
-#         print(chunk)
+# Streaming
+stream = llm.stream(messages=messages)
+for chunk in stream:
+    if chunk["choices"][0]["delta"]["content"]:
+        print(chunk)
 
 # No streaming
 llm = Gwenlake(model="meta/llama-3.1-8b-instruct")
@@ -28,7 +28,7 @@ response = llm.chat(messages=messages)
 print("")
 print(response["choices"][0]["message"]["content"])
 
-# stream = llm.stream(messages=messages)
-# for chunk in stream:
-#     if chunk["choices"][0]["delta"]["content"]:
-#         print(chunk)
+stream = llm.stream(messages=messages)
+for chunk in stream:
+    if chunk["choices"][0]["delta"]["content"]:
+        print(chunk)
