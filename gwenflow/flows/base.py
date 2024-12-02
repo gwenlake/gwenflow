99c35f3cfbd35de2cc277e9ebc954f9e08094cab
Sylvain Barthélémy <sylbarth@gmail.com>
2024-12-02 17:51:20 +0100

    Update base.py

--- gwenflow/flows/base.py
+++ gwenflow/flows/base.py
@@ -1,6 +1,7 @@
 
 from typing import List
 from pydantic import BaseModel
+import yaml
 
 from gwenflow.agents import Agent
 from gwenflow.utils import logger
@@ -13,6 +14,29 @@ class Flow(BaseModel):
     flow_type: str = "sequence"
 
 
+    @classmethod
+    def from_yaml(cls, file: str) -> "Flow":
+        if cls == Flow:
+            with open(file) as stream:
+                try:
+                    agents = []
+                    content_yaml = yaml.safe_load(stream)
+                    for name in content_yaml.get("agents").keys():
+                        _values = content_yaml["agents"][name]
+                        agent = Agent(
+                            name=name,
+                            role=_values.get("role"),
+                            task=_values.get("task"),
+                            response_model=_values.get("response_model"),
+                            # tools=[tool_wikipedia],
+                        )
+                        agents.append(agent)
+                    return Flow(agents=agents)
+                except Exception as e:
+                    logger.error(repr(e))
+
+        raise NotImplementedError(f"from_yaml not implemented for {cls.__name__}")
+    
     def run(self, message: str) -> str:
 
         context = None
