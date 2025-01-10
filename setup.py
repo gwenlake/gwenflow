210dc63b2f14718d2294907e38ce2f346421b83a
Antoine de Parthenay <88001989+Antoine-de-Parthenay@users.noreply.github.com>
2025-01-10 11:34:08 +0100

    Update setup.py (#9)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="gwenflow",
     description = "A framework for orchestrating applications powered by autonomous AI agents and LLMs.",
-    version="0.4.2",
+    version="0.4.3",
     url="https://github.com/gwenlake/gwenflow",
     author="The Gwenlake Team",
     author_email="info@gwenlake.com",
