29bde340605216fdec641595436cc45335730ba7
Antoine de Parthenay <antoine.departhenay@gwenlake.com>
2025-02-03 17:43:43 +0100

    Update website search

--- gwenflow/readers/website.py
+++ gwenflow/readers/website.py
@@ -1,26 +1,28 @@
-
-import time
 import random
-import httpx
+import time
+from typing import Set, Dict, List, Tuple
 from urllib.parse import urljoin, urlparse
 
-from typing import Set, Dict, List, Tuple
+import httpx
 
-from gwenflow.types import Document
+from gwenflow.readers import PDFReader
 from gwenflow.readers.base import Reader
+from gwenflow.types import Document
 from gwenflow.utils import logger
 
 try:
     from bs4 import BeautifulSoup  # noqa: F401
 except ImportError:
-    raise ImportError("BeautifulSoup is not installed. Please install it with `pip install beautifulsoup4`.")
+    raise ImportError(
+        "BeautifulSoup is not installed. Please install it with `pip install beautifulsoup4`."
+    )
 
 
 class WebsiteReader(Reader):
     """Reader for Websites"""
 
     max_depth: int = 3
-    max_links: int = 10
+    max_links: int = 100000
 
     delay: bool = True
 
@@ -37,7 +39,7 @@ class WebsiteReader(Reader):
         domain_parts = urlparse(url).netloc.split(".")
         return ".".join(domain_parts[-2:])
 
-    def _extract_main_content(self, soup: BeautifulSoup) -> str:
+    def _extract_html_content(self, soup: BeautifulSoup) -> str:
         """Extracts the main content from a BeautifulSoup object."""
 
         for tag in ["article", "main"]:
@@ -52,6 +54,16 @@ class WebsiteReader(Reader):
 
         return ""
 
+    def _extract_pdf_content(self, url: str) -> str:
+        """Extracts the main content from a PDF file."""
+        try:
+            documents = PDFReader().read(url)
+            content = [document.content for document in documents]
+            content = "\n\n".join(content)
+            return content
+        except:
+            return ""
+
     def crawl(self, url: str, starting_depth: int = 1) -> Dict[str, str]:
         """Crawls an url and returns a dictionary of URLs and their corresponding content."""
 
@@ -62,10 +74,10 @@ class WebsiteReader(Reader):
         self._urls_to_crawl.append((url, starting_depth))
 
         headers = {
-            'accept': '*/*',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53',
-            'Accept-Language': 'en-US,en;q=0.9,it;q=0.8,es;q=0.7',
-            'referer': 'https://www.google.com/',
+            "accept": "*/*",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53",
+            "Accept-Language": "en-US,en;q=0.9,it;q=0.8,es;q=0.7",
+            "referer": "https://www.google.com/",
         }
 
         with httpx.Client() as client:
@@ -97,21 +109,31 @@ class WebsiteReader(Reader):
                     soup = BeautifulSoup(response.content, "html.parser")
 
                     # Extract main content
-                    main_content = self._extract_main_content(soup)
-                    if main_content:
-                        crawler_result[current_url] = main_content
-                        num_links += 1
+                    if current_url.endswith(".pdf"):
+                        main_content = self._extract_pdf_content(current_url)
+                    else:
+                        main_content = self._extract_html_content(soup)
+
+                    crawler_result[current_url] = main_content
+                    num_links += 1
 
                     # Add found URLs to the global list, with incremented depth
                     for link in soup.find_all("a", href=True):
                         full_url = urljoin(current_url, link["href"])
                         parsed_url = urlparse(full_url)
+
                         # TODO: adjust to use PDFReader and TextReader if files are found
                         if parsed_url.netloc.endswith(primary_domain) and not any(
-                            parsed_url.path.endswith(ext) for ext in [".pdf", ".jpg", ".png"]
+                            parsed_url.path.endswith(ext) for ext in [".jpg", ".png"]
                         ):
-                            if full_url not in self._visited and (full_url, current_depth + 1) not in self._urls_to_crawl:
-                                self._urls_to_crawl.append((full_url, current_depth + 1))
+                            if (
+                                full_url not in self._visited
+                                and (full_url, current_depth + 1)
+                                not in self._urls_to_crawl
+                            ):
+                                self._urls_to_crawl.append(
+                                    (full_url, current_depth + 1)
+                                )
 
                 except Exception as e:
                     logger.debug(f"Failed to crawl: {current_url}: {e}")
@@ -134,5 +156,5 @@ class WebsiteReader(Reader):
                     metadata={"url": str(url)},
                 )
             )
-        
+
         return documents
