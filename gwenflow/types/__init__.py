e98452be2222b724e3798c4a226c4efaf2164409
Sylvain Barthélémy <sylbarth@gmail.com>
2025-02-17 00:32:15 +0100

    Models improvements

--- gwenflow/types/__init__.py
+++ gwenflow/types/__init__.py
@@ -1,11 +1,9 @@
-from gwenflow.types.chat_message import ChatMessage, messages_to_dict, messages_to_openai
-from gwenflow.types.completion import ChatCompletion, ChatCompletionChunk, ChatCompletionMessage, ChatCompletionMessageToolCall, Function
+from gwenflow.types.chat_message import ChatMessage
+from gwenflow.types.chat_completion import ChatCompletion, ChatCompletionChunk, ChatCompletionMessage, ChatCompletionMessageToolCall, Function
 from gwenflow.types.document import Document
 
 
 __all__ = [
-    "messages_to_dict",
-    "messages_to_openai",
     "ChatMessage",
     "ChatCompletion",
     "ChatCompletionChunk",
