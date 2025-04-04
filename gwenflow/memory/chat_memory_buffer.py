c68d87c8959d8c53220d95154b4391d05f7e4f2d
Sylvain Barthélémy <sylbarth@gmail.com>
2025-04-04 15:04:47 +0200

    Chat memory buffer token limit

--- gwenflow/memory/chat_memory_buffer.py
+++ gwenflow/memory/chat_memory_buffer.py
@@ -3,6 +3,7 @@ from pydantic import field_validator, Field
 
 from gwenflow.types import Message
 from gwenflow.memory.base import BaseChatMemory
+from gwenflow.logger import logger
 
 
 DEFAULT_TOKEN_LIMIT = 8192
@@ -18,57 +19,6 @@ class ChatMemoryBuffer(BaseChatMemory):
         token_limit = v or int(DEFAULT_TOKEN_LIMIT * DEFAULT_TOKEN_LIMIT_RATIO)
         return token_limit
     
-    def keep_tokens_from_text(self, text: str, token_limit= int):
-        num_tokens = 0
-        truncated_text = []
-        for token in text.split(" "):
-            num_tokens = self.tokenizer_fn(" ".join(truncated_text))
-            if num_tokens > token_limit:
-                break
-            truncated_text.append(token)
-        return " ".join(truncated_text)
-
-    def _get_truncated_messages(self, messages: list):
-
-        chat_history = []
-        token_count = 0
-
-        if self.system_prompt:
-            token_count = self._token_count_for_messages([Message(role="system", content=self.system_prompt)])
-        if token_count > self.token_limit:
-            raise ValueError("Initial token count exceeds token limit")
-
-        last_user_message = False
-        last_assistant_message = False
-
-        for message in reversed(messages):
-
-            if not last_user_message and message.role == "user":
-                chat_history.append(message)
-                last_user_message = True
-
-            if not last_assistant_message and message.role == "assistant":
-                chat_history.append(message)
-                last_assistant_message = True
-
-            if last_user_message and last_assistant_message:
-                break
-
-        if self.system_prompt:
-            chat_history.append(Message(role="system", content=self.system_prompt))
-
-        chat_history = list(reversed(chat_history))
-
-        token_count = self._token_count_for_messages(chat_history)
-        
-        # truncate last message
-        if token_count > self.token_limit:
-            token_limit = self.token_limit - self._token_count_for_messages(chat_history[:-1])
-            chat_history[-1].content = self.keep_tokens_from_text(chat_history[-1].content, token_limit=token_limit)
-
-        return chat_history
-
-
     def get(self):
 
         initial_token_count = 0
@@ -91,14 +41,10 @@ class ChatMemoryBuffer(BaseChatMemory):
             cur_messages = chat_history[-message_count:]
             token_count = self._token_count_for_messages(cur_messages) + initial_token_count
 
-        # we are above the limit. keep last message user and assistant message and truncate last assistant message.
-        if token_count > self.token_limit:
-            return self._get_truncated_messages(messages=chat_history)
-
-        # catch one message longer than token limit
-        # if token_count > self.token_limit or (message_count <= 0 and not self.system_prompt):
-        #     return []
-
+        if token_count > self.token_limit and not self.system_prompt:
+            logger.warning("Token limit exceeded.")
+            return []
+ 
         if self.system_prompt:
             return [Message(role="system", content=self.system_prompt)] + chat_history[-message_count:]
         
