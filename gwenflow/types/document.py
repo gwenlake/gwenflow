ac7a554c700bb1ad008667518229cba309ab0077
Thomas-Girault <thomas.girault@gwenlake.com>
2025-07-16 10:57:06 +0200

    Update file deletion system and Document class structure

--- gwenflow/types/document.py
+++ gwenflow/types/document.py
@@ -1,8 +1,9 @@
 
+import hashlib
 import uuid
 import enum
 from typing import Optional, Any, Dict, List
-from pydantic import BaseModel, ConfigDict, field_validator, Field
+from pydantic import BaseModel, ConfigDict, field_validator, Field, model_validator
 
 
 class DocumentCreationMode(str, enum.Enum):
@@ -15,19 +16,19 @@ class DocumentCreationMode(str, enum.Enum):
 class Document(BaseModel):
     """Base class for Documents"""
 
-    id: Optional[str] = Field(None, validate_default=True)
+    id: Optional[str] = None
     content: str
     metadata: Dict[str, Any] = {}
-    chunk_id: Optional[str] = None
     embedding: List[float] = None
     score: Optional[float] = None
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    @field_validator("id", mode="before")
-    def set_id(cls, v: Optional[str]) -> str:
-        id = v or str(uuid.uuid4())
-        return id
+    @model_validator(mode="after")
+    def set_id(self) -> "Document":
+        if self.id is None:
+            self.id = hashlib.md5(self.content.encode(), usedforsecurity=False).hexdigest()
+        return self
 
     def to_dict(self) -> Dict[str, Any]:
         """Returns a dictionary representation of the document"""
