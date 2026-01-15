29fcc5dc87a691710a6a978fe49d1275ef488688
mathysrattiname-gwen <mathys.rattiname@gwenlake.com>
2026-01-15 11:14:34 +0100

    add telemetry to example

--- examples/agent_web.py
+++ examples/agent_web.py
@@ -1,18 +1,23 @@
 import dotenv
 
-from gwenflow import Agent, ChatOpenAI
-from gwenflow.tools import PDFTool, WebsiteTool
-from gwenflow.utils import set_log_level_to_debug
+from gwenflow import Agent, ChatOpenAI, TelemetryBase, set_log_level_to_debug
+from gwenflow.tools import PDFReaderTool, WebsiteReaderTool
 
 set_log_level_to_debug()
 
 dotenv.load_dotenv(override=True)
 
+telemetry = TelemetryBase(service_name="gwenflow-v1",
+                          enabled=True,
+                          endpoint='http://localhost:6006/v1/traces')
+
+telemetry.setup_telemetry()
+telemetry.add_exporter()
 
 agent = Agent(
     name="Website explorer",
     llm=ChatOpenAI(model="gpt-4o-mini"),
-    tools=[WebsiteTool(), PDFTool()],
+    tools=[WebsiteReaderTool(), PDFReaderTool()],
 )
 
 response = agent.run("Summarize the activity of the following company: https://gwenlake.com.")
