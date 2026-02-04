d381191ad00aaa076a09a7b0d08cb28f453c4705
Sylvain Barthélémy <sylvain.barthelemy@gwenlake.com>
2026-02-04 08:53:56 +0100

    ToolCall

--- gwenflow/types/__init__.py
+++ gwenflow/types/__init__.py
@@ -1,8 +1,7 @@
 from gwenflow.types.message import Message
 from gwenflow.types.document import Document
 from gwenflow.types.usage import Usage
-from gwenflow.types.response import ModelResponse, AgentResponse
-from gwenflow.types.tool import Function, ToolCall, ToolResponse
+from gwenflow.types.response import Function, ToolCall, ToolResponse, ModelResponse, AgentResponse
 from gwenflow.types.items import ItemHelpers
 
 __all__ = [
